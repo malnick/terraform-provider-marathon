e0f0387daab27a0117b1f5d5d2673eec4c47e284
Jeff Malnick <malnick@Jeffs-MacBook-Pro.local>
2017-06-30 20:32:38 -0700

    Create a provider for the Marathon API

--- marathon/provider.go
+++ marathon/provider.go
@@ -1,6 +1,7 @@
 package marathon
 
 import (
+	marathon "github.com/gambol99/go-marathon"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -11,20 +12,14 @@ func Provider() terraform.ResourceProvider {
 		Schema: map[string]*schema.Schema{
 			"host": {
 				Type:        schema.TypeString,
-				Optional:    true,
+				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("MARATHON_HOST", ""),
 				Description: "The hostname where Marathon is running.",
-			},
-			"insecure": {
-				Type:        schema.TypeBool,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("MARATHON_INSECURE", false),
-				Description: "Whether server should be accessed without verifying the TLS certificate.",
 			},
 			"cluster_ca_certificate": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("MARATHON_CA_CERT_DATA", ""),
+				DefaultFunc: schema.EnvDefaultFunc("MARATHON_CA_CERT", ""),
 				Description: "PEM-encoded root certificates bundle for TLS authentication.",
 			},
 		},
@@ -32,5 +27,18 @@ func Provider() terraform.ResourceProvider {
 		ResourcesMap: map[string]*schema.Resource{
 			"marathon_app": resourceMarathonApp(),
 		},
+
+		ConfigureFunc: configure,
 	}
+}
+
+func configure(d *schema.ResourceData) (interface{}, error) {
+	config := marathon.NewDefaultConfig()
+	config.URL = d.Get("host").(string)
+	client, err := marathon.NewClient(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
